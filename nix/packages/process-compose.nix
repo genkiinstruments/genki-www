161c877b0a30adf7f4bbf1b049b2e1ba1cbff0c0
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-02-28 18:14:38 +0000

    feat: add catppuccin theme...

--- nix/packages/process-compose.nix
+++ nix/packages/process-compose.nix
@@ -18,8 +18,18 @@ let
             command: tailscale funnel localhost:5173
       '';
   };
+  catppuccinRepo = pkgs.fetchFromGitHub {
+    owner = "catppuccin";
+    repo = "process-compose";
+    rev = "b0c48aa07244a8ed6a7d339a9b9265a3b561464d";
+    sha256 = "sha256-uqJR9OPrlbFVnWvI3vR8iZZyPSD3heI3Eky4aFdT0Qo=";
+  };
+  themeDir = pkgs.runCommand "process-compose-theme" { } ''
+    mkdir -p $out/process-compose
+    cp ${catppuccinRepo}/themes/catppuccin-mocha.yaml $out/process-compose/theme.yaml
+  '';
   runScript = pkgs.writeShellScriptBin name ''
-    ${pkgs.process-compose}/bin/process-compose --config ${processComposeConfig}
+    ${pkgs.process-compose}/bin/process-compose --config ${processComposeConfig} --theme "Custom Style"
   '';
 in
 pkgs.symlinkJoin {
@@ -39,6 +49,7 @@ pkgs.symlinkJoin {
           pkgs.nodejs
           pkgs.tailscale
         ]
-      }
+      } \
+      --set XDG_CONFIG_HOME ${themeDir}
   '';
 }
