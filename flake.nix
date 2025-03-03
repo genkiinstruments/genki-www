60fd7c8ccf66a8b82d0f299c92fa7c1db3cf5c69
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-03-03 08:35:41 +0000

    feat: test for buildbot

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   # Add all your dependencies here
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
   };
