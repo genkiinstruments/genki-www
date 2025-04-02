8866eee8efb084aff498c86727c643604f5b6600
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-04-02 11:06:30 +0000

    feat: foo bar

--- src/routes/wave/technical-specifications/+page.svelte
+++ src/routes/wave/technical-specifications/+page.svelte
@@ -62,7 +62,7 @@
               <dt class="text-sm/6 font-medium text-gray-900">About</dt>
               <dd class="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
                 Fugiat ipsum ipsum deserunt culpa aute sint do nostrud anim incididunt cillum culpa consequat. Excepteur qui ipsum aliquip consequat sint. Sit id mollit nulla mollit nostrud in ea
-                officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu.
+                officia proident. Irure nostrud pariatur mollit ad adipisicing reprehenderit deserunt qui eu. Foo bar.
               </dd>
             </div>
             <div class="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
