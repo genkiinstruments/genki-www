460cdd3badc1bbdf69182d48182461b7684c5415
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-04-16 19:14:20 +0000

    feat(softwave): basic layout

--- src/routes/softwave/+page.svelte
+++ src/routes/softwave/+page.svelte
@@ -13,15 +13,44 @@
   let isRenderImage = $state(false);
 </script>
 
-<Header />
+<div class="@container mx-auto flex h-[100vh] w-full flex-1 flex-col px-5 md:px-[50px]">
+  <Header />
 
-<div class="bg-white">
-  <div class="pt-6">
-    <div class="mx-auto mt-8 max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8"></div>
+  <div class="flex min-h-[100vh] flex-col items-center justify-center py-20">
+    <div class="grid w-full grid-cols-10 items-center gap-10">
+      <div class="relative col-span-4 col-start-2" role="button" onmouseenter={() => (isRenderImage = true)} onmouseleave={() => (isRenderImage = false)} aria-roledescription="slide" tabindex="0">
+        <img src={softwave_hero} alt="Wave ring for musicians" class="w-full transition-opacity duration-300" class:opacity-0={isRenderImage} />
+        {#if isRenderImage}
+          <div class="absolute inset-0 flex items-center justify-center">
+            <img src={softwave_hero} alt="Wave ring close-up render" class="w-full" transition:fade={{ duration: 300 }} />
+          </div>
+        {/if}
+      </div>
+      <div class="col-span-3 col-start-7 md:text-2xl lg:text-4xl">
+        <div>
+          <div class="font-bold uppercase">Softwave</div>
+          <br />
+          <div>The key that unlocks the full potential of your Wave Ring. Control music with gestures, customize each button, and create saved presets from one seamlessly integrated application.</div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
+<img src={toti} alt="Bergur Torisson in the studio" class="w-full object-cover" />
+<div class="@container -mt-4 grid grid-cols-10">
+  <div class="col-span-1 col-start-3">
+    <img src={quote} alt="Quotation mark" />
+  </div>
+</div>
+<div class="grid grid-cols-10 pt-7">
+  <div class="col-span-3 col-start-3 text-4xl">The depth of this software knows no bounds</div>
+  <div class="col-span-2 col-start-8 flex flex-col space-y-2">
+    <div class=" text-4xl">Tóti Guðnason</div>
+    <div class=" text-sm">Music composer, producer and musician</div>
+  </div>
+</div>
 
-<YouTubeCarousel ids={["TB789hiIJfA", "zgtGceSGlHg", "1P4O6aG_XWk", "6EdUOi37k10"]} />
+<YouTubeCarousel ids={["TB789hiIJfA", "a8aStUjS6uk", "1P4O6aG_XWk", "6EdUOi37k10"]} />
 
 <Footer />
