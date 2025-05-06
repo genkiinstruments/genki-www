6e8a88d0ec7b4735e0a579692dfc6c1e86230a32
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-05-06 02:14:10 +0000

    feat(Testimonial): styling

--- src/lib/Testimonial.svelte
+++ src/lib/Testimonial.svelte
@@ -4,19 +4,17 @@
   const { src, mobileSrc, quote, name, occupation, alt } = $props();
 </script>
 
-<div class="w-full overflow-hidden">
+<div class="col-span-full w-full overflow-hidden">
   <img {src} {alt} class="hidden w-full object-cover sm:block" />
   <img src={mobileSrc ? mobileSrc : src} {alt} class="block w-full object-cover sm:hidden" />
 </div>
-<div class="-mt-10 grid grid-cols-10">
-  <div class="col-span-3 col-start-1 md:col-span-1 md:col-start-3">
+<div class="grid grid-cols-10 space-y-4 px-[50px]">
+  <div class="col-span-3 col-start-1 -mt-12 md:col-span-1 md:col-start-3">
     <img src={quote_mark} alt="Quotation mark" />
   </div>
-</div>
-<div class="grid grid-cols-10 gap-4 px-5 pt-7 md:gap-0 md:px-0">
-  <div class="col-span-8 col-start-3 text-xl md:col-span-3 md:col-start-3 md:text-4xl">{quote}</div>
-  <div class="col-span-8 col-start-3 mt-4 space-y-2 md:col-span-2 md:col-start-8 md:mt-0 md:flex md:flex-col">
+  <div class="md:4xl col-span-7 col-start-3 text-xl md:col-span-3 md:col-start-3 md:text-4xl">{quote}</div>
+  <div class="col-span-7 col-start-3 space-y-4 text-xl md:col-span-2 md:col-start-7">
     <div class="text-xl md:text-4xl">{name}</div>
-    <div class="text-lg leading-tight opacity-50 md:text-base">{occupation}</div>
+    <div class="text-lg leading-tight opacity-50">{occupation}</div>
   </div>
 </div>
