ad04c9aa4b646f0b58765293108c6211d4df329b
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-05-06 02:12:16 +0000

    feat(katla): feature sections

--- src/routes/katla/+page.svelte
+++ src/routes/katla/+page.svelte
@@ -34,4 +34,57 @@
 
 <YouTubeCarousel class="w-full overflow-hidden py-40 md:py-68" ids={["vPgHiyOXmMQ", "By-shXpcjSg", "BpfhsBbgY8M"]} />
 
+<div class="space-y-24">
+  <div class="w-full border-b border-[#F6F6F6] py-1 opacity-30"></div>
+
+  <div class="grid grid-cols-10 md:px-[50px]">
+    <p class="cap col-span-2 col-start-3 text-xl uppercase">Key features</p>
+    <ul class="col-span-3 col-start-7 space-y-8 text-lg capitalize">
+      <li>→ Five digiloc voices</li>
+      <li>→ Handmade basalt side-panels</li>
+      <li>→ Paraphonic-inpired polyphony</li>
+      <li>→ Volcanic modulation matrix</li>
+      <li>→ 80s inspired digital reverb</li>
+      <li>→ Analog wavefolder and distortion</li>
+      <li>→ Mountable in standard rack</li>
+    </ul>
+  </div>
+
+  <div class="w-full border-b border-[#F6F6F6] py-1 opacity-30"></div>
+
+  <div class="grid grid-cols-10 md:px-[50px]">
+    <p class="col-span-2 col-start-3 text-xl uppercase">Hardware</p>
+    <ul class="col-span-3 col-start-7 space-y-8 text-lg capitalize">
+      <li>→ Lava rock knobs</li>
+      <li>→ Lava rock side-panels</li>
+      <li>→ Aluminium enclosure</li>
+      <!-- [TODO]: Update with real dimenstions and weight (May 06, 2025 00:17, Oli) -->
+      <li>→ Dimensions are 43cm x 33cm x 25cm</li>
+      <li>→ Roughly 3kg</li>
+    </ul>
+  </div>
+
+  <div class="w-full border-b border-[#F6F6F6] py-1 opacity-30"></div>
+
+  <div class="grid grid-cols-10 md:px-[50px]">
+    <p class="col-span-2 col-start-3 text-xl uppercase">Two preset modes</p>
+    <ul class="col-span-3 col-start-7 space-y-8 text-lg capitalize">
+      <li>→ Store full panel as user preset</li>
+      <li>→ Store each row as row preset</li>
+    </ul>
+  </div>
+
+  <div class="w-full border-b border-[#F6F6F6] py-1 opacity-30"></div>
+
+  <div class="grid grid-cols-10 md:px-[50px]">
+    <p class="col-span-2 col-start-3 text-xl uppercase">IN THE BOX</p>
+    <ul class="col-span-3 col-start-7 space-y-8 text-lg capitalize">
+      <li>→ KATLA</li>
+      <li>→ USB-C cable power supply</li>
+      <li>→ 2x rack ears</li>
+      <li>→ Your own piece of Iceland</li>
+    </ul>
+  </div>
+</div>
+
 <Footer />
