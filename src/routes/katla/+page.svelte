7c602dbaa2ad27f264877669917eed2ee5c1a6fe
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-06-02 16:23:18 +0000

    feat(katla): 6 sold

--- src/routes/katla/+page.svelte
+++ src/routes/katla/+page.svelte
@@ -11,7 +11,7 @@
   import ScrollIndicator from "$lib/ScrollIndicator.svelte";
   import InteractiveString from "$lib/InteractiveString.svelte";
 
-  const sold = 5;
+  const sold = 6;
 
   type ListItemProps = {
     title: string;
