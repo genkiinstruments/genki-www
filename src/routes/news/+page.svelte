91c49e51b8d3a45ec72dcbe268a25949f3635d63
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2025-08-29 16:00:38 +0000

    feat(news): add page

---
+++ src/routes/news/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Header from "$lib/Header.svelte";
+  import Footer from "$lib/Footer.svelte";
+</script>
+
+<div class="relative mx-auto flex w-full flex-1 flex-col px-5">
+  <Header />
+</div>
+
+<Footer />
